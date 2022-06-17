276f01b83190925756a24cb4cbc075a60d9d2ec7
kevtie <tiskevins@gmail.com>
2022-06-17 14:49:01 +0200

    route update

--- routes/web.php
+++ routes/web.php
@@ -23,13 +23,13 @@ Route::get('/', function () {
 });
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
-Route::post('/login/authenticate', [LoginController::class, 'authenticate'])->name('auth');
+Route::match(['get', 'post'], '/login/authenticate', [LoginController::class, 'authenticate'])->name('auth');
 Route::get('/home', [PageController::class, 'home'])->name('home')->middleware('checklogin');
 Route::get('/products', [PageController::class, 'products'])->name('product')->middleware('checklogin');
 Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
-Route::post('/products/addproduct', [OrderController::class, 'addToCart'])->name('addtocart')->middleware('checklogin');
-Route::post('/products/removeproduct', [OrderController::class, 'removeFromCart'])->name('removefromcart')->middleware('checklogin');
+Route::match(['get', 'post'], '/products/addproduct', [OrderController::class, 'addToCart'])->name('addtocart')->middleware('checklogin');
+Route::match(['get', 'post'], '/products/removeproduct', [OrderController::class, 'removeFromCart'])->name('removefromcart')->middleware('checklogin');
 Route::get('/order', [PageController::class, 'order'])->name('order')->middleware('checklogin');
-Route::post('/order/buy', [OrderController::class, 'payment'])->name('pay')->middleware('checklogin');
+Route::match(['get', 'post'], '/order/buy', [OrderController::class, 'payment'])->name('pay')->middleware('checklogin');
 Route::get('/orderhistory', function(){return view('orderHistory');})->name('orderhistory')->middleware('checklogin');
 Route::get('/products/details/{product}', [ProductDetailController::class, 'getProductPage'])->name('productdetail')->middleware('checklogin');
