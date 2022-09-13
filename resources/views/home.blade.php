081703e2b3944f1d4992f5c39aed87b2932c0d0a
kevtie <tiskevins@gmail.com>
2022-09-13 11:38:30 +0200

    added dropdown menu to nav

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,8 +7,7 @@ use App\Http\Controllers\OrderController;
 @extends('layouts.app')
 
 @section('content')
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
-<div class="container">
+
     <div class="row justify-content-center">
         <div class="col-md-8">
           <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,11 +4,11 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
+<header>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
@@ -22,43 +22,51 @@
                             </li>
                         @endforeach
                     </ul>
-                    <ul class="navbar-nav">
+                    <ul class="navbar-nav ">
                       <li class="navbar-text">
                         <form method="post" action="{{ route('showSearch') }}">
                           @csrf
-                        <div class="input-group mb-3">
-                        <input type="text" class="form-control" placeholder="Search..." id="search" name="search">
-                        <div class="input-group-append">
-                        <input class="btn btn-outline-secondary" type="submit" value="search">
-                        </div>
-                      </div>
-                    </form>
+                          <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="Search..." id="search" name="search">
+                            <div class="input-group-append">
+                              <input class="btn btn-outline-secondary" type="submit" value="search">
+                            </div>
+                          </div>
+                        </form>
                       </li>
+                    </ul>
+                    <ul class="navbar-nav">
                       <li class="navbar-text">
                         <span class="navbar-text flex row mx-2">
-                          <p>Welcome!
-                            <div class="dropdown show">
-                              <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                          @if(Auth::check() == true)
+                          <p class="flex">Welcome!
+                            <div class="dropdown">
+                              <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
                                 {{Auth::user()->name}}
                               </button>
-                              <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                                <a class="dropdown-item" href="{{route('profile', ['name' => Auth::user()->name])}}">Profile</a>
-                                <a class="dropdown-item" href="#">Another action</a>
-                                <a class="dropdown-item" href="{{route('logout')}}">logout</a>
-                              </div>
+                              <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                <li><a class="dropdown-item" href="{{route('profile', ['name' => Auth::user()->name])}}">Profile</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><a class="dropdown-item" href="{{route('logout')}}">logout</a></li>
+                              </ul>
                             </div>
                           </p>
+                            @else
+                            <div class="show">
+                              <a class="btn" href="{{route('login')}}">Login</a>
+                            </div>
+                            @endif
                         </span>
-                        <!---<a href="{{route('profile', ['name' => Auth::user()->name])}}" style="text-decoration: none; color: inherit;">{{Auth::user()->name}}</a>--->
+
                       </li>
                     </ul>
                 </div>
             </div>
         </nav>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
     </div>
-</body>
+</header>
+<main class="py-4">
+  @yield('content')
+</main>
 </html>
